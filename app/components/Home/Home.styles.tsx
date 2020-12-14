b1c4d07ea04742ca2e9fc6323fca4fa231c40838
Kamil <kamil.pieczyk@gmail.com>
2020-12-14 09:41:46 +0000

    ADD HOME COMPONENT (ORDER LIST)

--- app/components/Home/Home.styles.tsx
+++ app/components/Home/Home.styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import colors from '../../constants/colors';
 
 export const Container = styled.div`
   width: 100%;
@@ -6,4 +7,19 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+`;
+
+export const LoadingContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const OrderContainer = styled.div`
+  overflow-x: hidden;
+  overflow-y: auto;
+  width: 100%;
+  height: 550px;
 `;
--- app/components/Home/Home.view.tsx
+++ app/components/Home/Home.view.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { ProgressCircle } from 'react-desktop/windows';
 
-import { Container } from './Home.styles';
+import { Container, LoadingContainer, OrderContainer } from './Home.styles';
+import { VievType, Order as OrderType } from './Home.types';
+import { red } from '../../constants/colors';
+import Order from './Order';
 
-const View: React.FunctionComponent = (p) => {
+const View: React.FunctionComponent<VievType> = (p) => {
   return (
     <Container>
       <h1>Orders list</h1>
+      {p.state.isLoading ? (
+        <LoadingContainer>
+          <ProgressCircle color={red} size={80} />
+        </LoadingContainer>
+      ) : (
+        <OrderContainer>
+          {p.state.orders.map((order: OrderType) => (
+            <Order key={order._id} {...order} />
+          ))}
+        </OrderContainer>
+      )}
     </Container>
   );
 };
