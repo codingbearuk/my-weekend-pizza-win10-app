6252d80da77edcd6ff6cd210097003f35e2d2dcc
Kamil <kamil.pieczyk@gmail.com>
2020-12-25 17:02:57 +0000

    ADD NEW PIZZA ADD COMPONENT

--- app/components/Add-new-pizza/Add-new-pizza.view.tsx
+++ app/components/Add-new-pizza/Add-new-pizza.view.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
-import { Container } from './Add-new-pizza.styles';
+import { Container, Content, Image } from './Add-new-pizza.styles';
+import key from '../../utils/api-comunication/key';
+import ImagePicker from '../UI/Image-picker';
 
-const View: React.FunctionComponent = (p) => {
+interface ViewType {
+  state: {
+    selectetImage: string | null;
+    isSelectImageContainerOpen: boolean;
+    images: string[];
+  };
+  handlers: {
+    handleImageSelectContainer: VoidFunction;
+    handleSelectImage: (selectedImage: string) => void;
+  };
+}
+
+const View: React.FunctionComponent<ViewType> = (p) => {
   return (
     <Container>
       <h1>Add new pizza</h1>
+      <Content>
+        <Image
+          src={
+            p.state.selectetImage
+              ? `${key}/images/pizzas/${p.state.selectetImage}`
+              : `${key}/images/pizzas/pngegg-1.png`
+          }
+          isSelected={p.state.selectetImage ? true : false}
+          onClick={p.handlers.handleImageSelectContainer}
+        />
+        {p.state.isSelectImageContainerOpen && (
+          <ImagePicker
+            images={p.state.images}
+            callback={p.handlers.handleSelectImage}
+            mode="pizzas"
+          />
+        )}
+      </Content>
     </Container>
   );
 };
